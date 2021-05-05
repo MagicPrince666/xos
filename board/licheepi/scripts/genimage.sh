160d593ef1f74e96967ed3447bad26666a3576d0
huangliquan <huangliquan@xa.com>
2021-05-05 22:29:36 +0800

    add lcd suppost

--- board/licheepi/scripts/genimage.sh
+++ board/licheepi/scripts/genimage.sh
@@ -12,4 +12,3 @@ ${SELFDIR}/mknanduboot.sh ${1}/${2} ${1}/u-boot-sunxi-with-nand-spl.bin
 support/scripts/genimage.sh ${1} -c board/licheepi/genimage-sdcard.cfg
 support/scripts/genimage.sh ${1} -c board/licheepi/genimage-nor.cfg
 support/scripts/genimage.sh ${1} -c board/licheepi/genimage-nand.cfg
-# support/scripts/genimage.sh ${1} -c board/licheepi/genimage-flasher.cfg
