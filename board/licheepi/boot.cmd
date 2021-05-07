4d88e68d9796c034cdd351bd4ad9bcf39510a248
huangliquan <huangliquan@xa.com>
2021-05-07 13:50:58 +0800

    fix dts

--- board/licheepi/boot.cmd
+++ board/licheepi/boot.cmd
@@ -1,4 +1,4 @@
 setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait
 load mmc 0:1 0x41000000 zImage
-load mmc 0:1 0x41800000 sun8i-v3s-licheepi-zero.dtb
+load mmc 0:1 0x41800000 sun8i-v3s-prince.dtb
 bootz 0x41000000 - 0x41800000
