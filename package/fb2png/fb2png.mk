908965754013fbcb1f8740317dd14f93615d7768
huangliquan <huangliquan@xa.com>
2021-05-08 17:51:48 +0800

    fix name card

---
+++ package/fb2png/fb2png.mk
@@ -0,0 +1,37 @@
+################################################################################
+#
+# xag imu test
+# huangliquan@xa.com
+# 2020/10/9
+#
+################################################################################
+
+
+FB2PNG_VERSION = 1.0.0
+FB2PNG_SITE = $(TOPDIR)/package/fb2png/src
+FB2PNG_SITE_METHOD = local
+FB2PNG_INSTALL_TARGET = YES
+FB2PNG_DEPENDENCIES = 
+FB2PNG_DIR := $(BUILD_DIR)/fb2png
+
+define FB2PNG_BUILD_CMDS
+    make CC=$(TARGET_CC) CXX=$(TARGET_CXX) -C $(@D)
+endef
+
+define FB2PNG_CLEAN_CMDS
+    make -C $(@D) clean
+endef
+
+define FB2PNG_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/fb2png $(TARGET_DIR)/usr/bin
+endef
+
+define FB2PNG_PERMISSIONS
+    /usr/bin/fb2png f 4755 0 0 - - - - -
+endef
+
+define FB2PNG_UNINSTALL_TARGET_CMDS
+    $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) uninstall
+endef
+
+$(eval $(generic-package))
---
+++ package/spitft/spitft.mk
@@ -0,0 +1,37 @@
+################################################################################
+#
+# xag imu test
+# huangliquan@xa.com
+# 2020/10/9
+#
+################################################################################
+
+
+SPITFT_VERSION = 1.0.0
+SPITFT_SITE = $(TOPDIR)/package/spitft/src
+SPITFT_SITE_METHOD = local
+SPITFT_INSTALL_TARGET = YES
+SPITFT_DEPENDENCIES = 
+SPITFT_DIR := $(BUILD_DIR)/spitft
+
+define SPITFT_BUILD_CMDS
+    make CC=$(TARGET_CC) CXX=$(TARGET_CXX) -C $(@D)
+endef
+
+define SPITFT_CLEAN_CMDS
+    make -C $(@D) clean
+endef
+
+define SPITFT_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/spitft $(TARGET_DIR)/usr/bin
+endef
+
+define SPITFT_PERMISSIONS
+    /usr/bin/spitft f 4755 0 0 - - - - -
+endef
+
+define SPITFT_UNINSTALL_TARGET_CMDS
+    $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) uninstall
+endef
+
+$(eval $(generic-package))
