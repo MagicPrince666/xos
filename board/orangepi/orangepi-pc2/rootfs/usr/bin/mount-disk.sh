6a2e3e0cfedfb1eb70168ee954ca63edba378ae3
huangliquan <huangliquan@xa.com>
2021-05-13 17:52:30 +0800

    fix file

--- board/orangepi/orangepi-pc2/rootfs/usr/bin/mount-disk.sh
+++ board/orangepi/orangepi-pc2/rootfs/usr/bin/mount-disk.sh
@@ -2,7 +2,9 @@
 mkdir -p /media/$1
 mount /dev/$1 /media/$1
 
-if [ -f "/media/$1/c-box" ]; then
+if [ -f "/media/$1/c-box/checkra1n" ]; then
 	echo `Update checkra1n...`
 	cp -r /media/$1/c-box/* /root
+	sync
+	umount /media/$1
 fi
