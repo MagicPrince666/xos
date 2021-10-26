25de831359566052aba19cb0a265a28451503c96
MagicPrince666 <846863428@qq.com>
2021-10-26 22:54:51 +0800

    file

--- package/prince/myjpeg/myjpeg.mk
+++ package/prince/myjpeg/myjpeg.mk
@@ -25,6 +25,7 @@ endef
 define MYJPEG_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/myjpeg $(TARGET_DIR)/usr/bin
     $(INSTALL) -D -m 0755 $(@D)/S82showpic $(TARGET_DIR)/etc/init.d
+    $(INSTALL) -D -m 0755 $(@D)/pictrue.jpg $(TARGET_DIR)/root
 endef
 
 define MYJPEG_PERMISSIONS
