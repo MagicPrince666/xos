8fcafbae2ce6718a77875695527c4f25780afd9d
leo <hlq@ldrobot.com>
2021-10-27 19:50:49 +0800

    fix name

--- package/prince/mp4player/mp4player.mk
+++ package/prince/mp4player/mp4player.mk
@@ -24,7 +24,7 @@ endef
 
 define MP4PLAYER_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/mp4player $(TARGET_DIR)/usr/bin
-    $(INSTALL) -D -m 0755 $(@D)/bigbuckbunny_480x272.h264 $(TARGET_DIR)/root
+    $(INSTALL) -D -m 0755 $(@D)/bigbuckbunny_320x240.h264 $(TARGET_DIR)/root
 endef
 
 define MP4PLAYER_PERMISSIONS
