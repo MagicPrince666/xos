bc902276995c505b8b2d7ef31668f83b03eaf690
MagicPrince666 <846863428@qq.com>
2021-12-16 00:45:04 +0800

    add pkgconfig

--- package/prince/libimobiledevice-glue/libimobiledevice-glue.mk
+++ package/prince/libimobiledevice-glue/libimobiledevice-glue.mk
@@ -14,12 +14,10 @@ LIBIMOBILEDEVICE_GLUE_DEPENDENCIES = libplist host-libtool host-autoconf host-au
 LIBIMOBILEDEVICE_GLUE_LICENSE = LGPL-2.1
 LIBIMOBILEDEVICE_GLUE_LICENSE_FILES = $(LIBIMOBILEDEVICE_GLUE_SUBDIR)/COPYING
 
+LIBIMOBILEDEVICE_CPE_ID_VENDOR = libimobiledevice
+
 define LIBIMOBILEDEVICE_GLUE_AVOID_AUTORECONF_HOOK
-	find $(@D) -name aclocal.m4 -exec touch '{}' \;
-	find $(@D) -name config-h.in -exec touch '{}' \;
-	find $(@D) -name configure -exec touch '{}' \;
-	find $(@D) -name Makefile.in -exec touch '{}' \;
-    cd $(@D) && ./autogen.sh --prefix=$(HOST_DIR)/usr/lib --host=$(GNU_TARGET_NAME) CC=$(TARGET_CC) CXX=$(TARGET_CXX)
+    cd $(@D) && PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig" ./autogen.sh --prefix=$(HOST_DIR)/usr/lib --host=$(GNU_TARGET_NAME) CC=$(TARGET_CC) CXX=$(TARGET_CXX)
 endef
 LIBIMOBILEDEVICE_GLUE_PRE_CONFIGURE_HOOKS += LIBIMOBILEDEVICE_GLUE_AVOID_AUTORECONF_HOOK
 
