131b0b4d2dea27fea8a16838317a0c9f0fea1646
MagicPrince666 <846863428@qq.com>
2023-01-11 22:18:30 +0800

    更新

--- package/prince/remotecar/remotecar.mk
+++ package/prince/remotecar/remotecar.mk
@@ -4,14 +4,14 @@
 #
 ################################################################################
 REMOTECAR_VERSION = master
-REMOTECAR_SITE = $(call github,MagicPrince666,f1c100scar,$(REMOTECAR_VERSION))
+REMOTECAR_SITE = $(call github,MagicPrince666,linux-car,$(REMOTECAR_VERSION))
 REMOTECAR_DEPENDENCIES = 
 REMOTECAR_INSTALL_STAGING = YES
 REMOTECAR_INSTALL_TARGET = YES
 
 REMOTECAR_CONF_OPTS =
 
-REMOTECAR_DIR := $(BUILD_DIR)/RemoteCar
+REMOTECAR_DIR := $(BUILD_DIR)/SmartCar
 
 define REMOTECAR_BUILD_CMDS
     make CC=$(TARGET_CC) CXX=$(TARGET_CXX) -C $(@D)
@@ -22,16 +22,16 @@ define REMOTECAR_CLEAN_CMDS
 endef
 
 define REMOTECAR_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/RemoteCar $(TARGET_DIR)/usr/sbin
+    $(INSTALL) -D -m 0755 $(@D)/SmartCar $(TARGET_DIR)/usr/sbin
     $(INSTALL) -D -m 0755 $(@D)/S99remotecar $(TARGET_DIR)/etc/init.d
 endef
 
 define REMOTECAR_PERMISSIONS
-    /usr/sbin/RemoteCar f 4755 0 0 - - - - -
+    /usr/sbin/SmartCar f 4755 0 0 - - - - -
 endef
 
 define REMOTECAR_UNINSTALL_TARGET_CMDS
     $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) uninstall
 endef
 
-$(eval $(generic-package))
+$(eval $(cmake-package))
